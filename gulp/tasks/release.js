34c4bb0b5d1f0cb9718a2bb0b8742107c878f12c
acierto <bogdannechiporenko@gmail.com>
2019-03-09 10:13:00 +0100

    Updated docs

--- gulp/tasks/release.js
+++ gulp/tasks/release.js
@@ -3,4 +3,4 @@ import {release} from 'gulp-release-it';
 
 release(gulp);
 
-gulp.task('complete-release', gulp.series('build-dist', 'bump-release'));
+gulp.task('complete-release', gulp.series('build-dist', 'doc', 'bump-complete-release'));
