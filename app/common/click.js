54a2242034a999d0d8c7e9c39a5852fd072fd045
acierto <bogdannechiporenko@gmail.com>
2019-06-26 12:45:18 +0200

    Added click util with andExpectDisplayed method

---
+++ app/common/click.js
@@ -0,0 +1,20 @@
+import {Action} from './action';
+import {ActionUtil} from './action-util';
+import {WaitCondition} from './wait-condition';
+
+/**
+ * Utility method to work with clicking.
+ */
+export class Click {
+    /**
+     *  Click on the element and expects it to be displayed
+     *
+     * @param clickSelector
+     * @param displayedSelector
+     */
+    andExpectDisplayed(clickSelector, displayedSelector) {
+        const action = () => Action.click(clickSelector);
+        const condition = () => WaitCondition.displayed(displayedSelector);
+        ActionUtil.repeatAction(action, condition);
+    }
+}
